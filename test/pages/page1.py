6015dbedddeaace280e943fb0eb022b6162ffc53
JJC3321 <122761865+JJC3321@users.noreply.github.com>
2024-05-02 11:40:12 -0400

    Add files via upload

---
+++ test/pages/page1.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from api import predict_tabular_classification_sample
+
+def all_params() :
+    st.subheader("Find out what crops/plants to grow.")
+    st.sidebar.success("Type of Data")
+    st.sidebar.page_link("mainapp.py", label="Home")
+    st.sidebar.page_link('pages/page1.py', label="Input All Fields")
+    st.sidebar.page_link('pages/page2.py', label="Location as Field")
+    Ncontent = st.text_input("Nitrogen in soil")
+    Pcontent = st.text_input("Phosphorous in soil")
+    Kcontent = st.text_input("Potassium in soil")
+    temperature = st.text_input("Temperature (Celcius)")
+    humidity = st.text_input("Humidity percentage")
+    ph = st.text_input("ph of soil")
+    rainfall = st.text_input("rainfall in mm")
+    if st.button("Submit") :
+        #call api here
+        st.write("") 
+        data = predict_tabular_classification_sample(instance_dict={
+            "N":Ncontent,
+            "P":Pcontent,
+            "K":Kcontent,
+            "temperature": temperature,
+            "humidity": humidity,
+            "ph":ph,
+            "rainfall": rainfall,
+        })
+        st.write(data)
+
+    #location = st.text_input("Where are you?")
+    #st.write(get_temp(location))
+    #get_temp("Alpharetta")
+    #get_humidity("Alpharetta")
+    #print(f'{round(get_temp("Alpharetta"), 2)}    {get_humidity("Alpharetta")}')
+st.set_page_config(page_title="All Fields", page_icon="📈")
+all_params()
